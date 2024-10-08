fcd5da9a549ce2a905d043be0da6f7788ea1f7f3
abelard-school-csc <amuratov@abelardschool.org>
2024-10-08 12:32:31 -0400

    version 1,1

--- harmonics.py
+++ harmonics.py
@@ -4,21 +4,26 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.special import sph_harm
 
 def spherical_harmonics(n, l, ml, ms):
-    theta, phi = np.mgrid[0:np.pi:100j, 0:2*np.pi:100j]
+    theta, phi = np.mgrid[0:np.pi:200j, 0:2*np.pi:200j] 
     r = np.abs(sph_harm(ml, l, phi, theta))**2
     x = r * np.sin(theta) * np.cos(phi)
     y = r * np.sin(theta) * np.sin(phi)
     z = r * np.cos(theta)
 
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(12, 10))
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(x, y, z, rstride=1, cstride=1, color='c', edgecolor='k', alpha=0.6)
-    ax.set_xlim([-0.5, 0.5])
-    ax.set_ylim([-0.5, 0.5])
-    ax.set_zlim([-0.5, 0.5])
+    
+    surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', edgecolor='none', alpha=0.8)
+
+    ax.view_init(elev=30, azim=30)
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
+    ax.set_zlim([-1, 1])
+    
     ax.set_box_aspect([1, 1, 1])
-    plt.title(f'Plot for Reaction {n}')
-    plt.savefig(f'spherical_harmonic_n{n}_l{l}_ml{ml}_ms{ms}.png')
+    plt.title(f'Spherical Harmonic for n={n}, l={l}, ml={ml}, ms={ms}', fontsize=16)
+    plt.colorbar(surf, shrink=0.5, aspect=5)
+    plt.savefig(f'spherical_harmonic_n{n}_l{l}_ml{ml}_ms{ms}.png', bbox_inches='tight')
 
 n = int(input("Enter principal quantum number (n): "))
 l = int(input("Enter angular momentum quantum number (l): "))
